ab8ad1d03df0083e5caa667ed0e3215a6fd68d08
armairashaffa <armairashaffa@gmail.com>
2025-08-13 22:09:37 +0700

    Add files via upload

---
+++ rianti/index-riri.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+</head>
+
+<body>
+    <header>
+        <h3>Pendaftaran Siswa Baru</h3>
+        <h1>SMK Coding</h1>
+    </header>
+
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar-riri.php">Daftar Baru</a></li>
+            <li><a href="list-siswa-riri.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+
+    </body>
+</html>
